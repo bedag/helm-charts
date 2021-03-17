7f71bfc1334cfcb2c85770033a4ba108de5d0f7d
Oliver Bähler <oliverbaehler@hotmail.com>
2021-03-17 01:08:27 +0100

    External workflow

--- scripts/changed-charts.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -o pipefail
-
-repository=$(git rev-parse --show-toplevel)
-: "${CT_CONFIG:-$repo_root/ct.yaml}"
-changed_charts=$(ct list-changed --config "${CT_CONFIG}")
-echo "$changed_charts"
